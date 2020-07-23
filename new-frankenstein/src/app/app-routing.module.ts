78f95bda37d7da641e734c848d4c3fab4d838efa
joseph Evangelista <oootep123@gmail.com>
2020-07-22 23:17:46 -0400

    added day2 component

--- new-frankenstein/src/app/app-routing.module.ts
+++ new-frankenstein/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { Day1Component } from './day1/day1.component';
+import { Day2Component } from './day2/day2.component';
 
 const routes: Routes = [
   { path: '', component: HeaderComponent },
   { path: '', component: Day1Component },
+  { path: '', component: Day2Component },
 ];
 
 @NgModule({
