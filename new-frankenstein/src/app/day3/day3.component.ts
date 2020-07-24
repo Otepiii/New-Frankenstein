0f8e3ca80008346c44bd16ad9bf610ec938997c0
joseph Evangelista <oootep123@gmail.com>
2020-07-24 00:28:26 -0400

    implemented day 3 component

--- new-frankenstein/src/app/day3/day3.component.ts
+++ new-frankenstein/src/app/day3/day3.component.ts
@@ -1,15 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+export interface RecipeList {
+  position: number;
+  name: string;
+  amount: number;
+  unit: string;
+  technique: string;
+}
+
+const ELEMENT_DATA: RecipeList[] = [
+  {
+    position: 1,
+    name: 'Chicken Thighs',
+    amount: 3,
+    unit: 'Pcs',
+    technique: 'Pan seared/fried',
+  },
+  {
+    position: 2,
+    name: 'Soy Sauce',
+    amount: 3,
+    unit: 'Tbs',
+    technique: 'reduced',
+  },
+  { position: 3, name: 'Mirin', amount: 3, unit: 'Tbs', technique: 'reduced' },
+  { position: 4, name: 'Sake', amount: 3, unit: 'Tbs', technique: 'reduced' },
+  { position: 5, name: 'Sugar', amount: 3, unit: 'Tbs', technique: 'reduced' },
+  { position: 6, name: 'Rice', amount: 2, unit: 'Cups', technique: 'Steamed' },
+];
 
 @Component({
   selector: 'app-day3',
   templateUrl: './day3.component.html',
-  styleUrls: ['./day3.component.scss']
+  styleUrls: ['./day3.component.scss'],
 })
-export class Day3Component implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class Day3Component {
+  displayedColumns: string[] = [
+    'position',
+    'name',
+    'amount',
+    'unit',
+    'technique',
+  ];
+  dataSource = ELEMENT_DATA;
 }
