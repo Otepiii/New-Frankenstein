d11f5f9cdf019fbbe004194870689d1d0c303a5c
joseph Evangelista <oootep123@gmail.com>
2020-07-28 23:54:02 -0400

    created day6 component

--- new-frankenstein/src/app/app.module.ts
+++ new-frankenstein/src/app/app.module.ts
@@ -17,6 +17,7 @@ import { Day3Component } from './day3/day3.component';
 import { DialogExampleComponent } from './dialog-example/dialog-example.component';
 import { Day4Component } from './day4/day4.component';
 import { Day5Component } from './day5/day5.component';
+import { Day6Component } from './day6/day6.component';
 
 @NgModule({
   declarations: [
@@ -28,6 +29,7 @@ import { Day5Component } from './day5/day5.component';
     DialogExampleComponent,
     Day4Component,
     Day5Component,
+    Day6Component,
   ],
   entryComponents: [DialogExampleComponent],
   imports: [
